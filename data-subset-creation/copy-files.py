92231a9e4b3c2c2a1743a47e3e0562470a8c9d7b
KarlTamberg <tamberg.karl@gmail.com>
2023-12-02 14:02:57 +0200

    Add/amend scripts; Added script for removing comments (requires testing). Amend copy scripts to include helper files.

--- data-subset-creation/copy-files.py
+++ data-subset-creation/copy-files.py
@@ -6,10 +6,6 @@ def process_file(root, file_path):
     new_root = root.replace("Java Juliet 1.3", "mini-testing")
     new_path = file_path.replace("Java Juliet 1.3", "mini-testing")
 
-    #answer = str(input("Copying " + file_path + " to " + new_path + ". Continue?"))
-    #if answer != "yes":
-    #    return
-
     os.makedirs(new_root, exist_ok=True)
     shutil.copy(file_path, new_path)
 
@@ -19,13 +15,13 @@ def process_directory(directory_path):
         for file in files:
             if "build.xml" == file or "copy-files.py" == file or "antbuild" in root:
                 continue
-            if "CWE" not in file or "_01" in file:
+            if "CWE" not in file or "_01" in file or "Helper" in file:
                 file_path = os.path.join(root, file)
                 process_file(root, file_path)
 
 
 """
-Run through all files in subdirectories and copy the lines not starting with 
+Run through all files in subdirectories and copy the files.
 """
 if __name__ == "__main__":
     current_directory = "C:\\Users\\karlt\\thesis\\datasets\\Java Juliet 1.3\\src\\testcases"  # os.getcwd()
---
+++ dataset-normalization/remove-comments.py
@@ -0,0 +1,31 @@
+import os
+import re
+
+
+def remove_comments(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Remove multiline comments
+    content = re.sub(r'/\*.*?\*/', '', content, flags=re.DOTALL)
+
+    # Remove single-line comments
+    content = re.sub(r'// .*', '', content)
+    content = re.sub(r' //.*', '', content)
+
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+def process_directory(directory_path):
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith(".java"):
+                file_path = os.path.join(root, file)
+                remove_comments(file_path)
+
+
+if __name__ == "__main__":
+    current_directory = "C:\\Users\\karlt\\thesis\\datasets\\mini-testing\\src\\testcases\\CWE23_Relative_Path_Traversal"  # os.getcwd()
+    process_directory(current_directory)
+    print("Script completed successfully.")
