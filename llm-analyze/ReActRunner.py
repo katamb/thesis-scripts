4329b9d4eb15811b31d0f8c995fea4e520255ad7
KarlTamberg <tamberg.karl@gmail.com>
2024-02-03 13:21:37 +0200

    Ready for 1st run;

--- llm-analyze/ReActRunner.py
+++ llm-analyze/ReActRunner.py
@@ -7,6 +7,7 @@ from AstTool import get_ast_from_code
 from ScatTool import get_codeql_results, get_spotbugs_results
 from langchain.agents import Tool
 from langchain_experimental.utilities import PythonREPL
+from langchain_core.runnables import RunnableConfig
 import time
 
 
@@ -29,7 +30,7 @@ class ReActRunner(BaseLlmRunner):
             description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
             func=python_repl.run,
         )
-        tools = [repl_tool, get_codeql_results, get_spotbugs_results, get_ast_from_code]  # todo ast does not work. The other one where code was provided worked better. Potentially pre-load this data? + add scat retriavl tool
+        tools = [repl_tool, get_codeql_results, get_spotbugs_results, get_ast_from_code]  # todo ast does not always work. The other one where code was provided worked better. Potentially pre-load this data? + add scat retriavl tool
 
         # todo callback not working atm
         with get_openai_callback() as cb:
@@ -39,7 +40,7 @@ class ReActRunner(BaseLlmRunner):
             agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
             # Make calls
             start = time.time()
-            llm_response = agent_executor.invoke({"code": code}, verbose=True)
+            llm_response = agent_executor.invoke({"code": code}, config=RunnableConfig(callbacks=[cb]), include_run_info=True, verbose=True)
             end = time.time()
             tokens_used = f"total_tokens: {cb.total_tokens}, completion_tokens: {cb.completion_tokens}, prompt_tokens: {cb.prompt_tokens}"
             cost = cb.total_cost
--- llm-analyze/main.py
+++ llm-analyze/main.py
@@ -1,47 +1,54 @@
 from dotenv import load_dotenv, find_dotenv
-from SimplePromptRunner import SimplePromptRunner
+# from SimplePromptRunner import SimplePromptRunner
+from ReActRunner import ReActRunner
 from SelfReflectionPromptRunner import SelfReflectionPromptRunner
+from CriticiseRefinePromptRunner import CriticiseRefinePromptRunner
 from concurrent.futures import ThreadPoolExecutor
 import os
 
 
 def run_prompt(file_path):
-    runner = SelfReflectionPromptRunner(file_path, "dataflow_analysis_prompt")
+    runner = CriticiseRefinePromptRunner(file_path, "basic_prompt_rci", "basic_prompt_rci_criticise", "basic_prompt_rci_improve")
     runner.run_prompt()
 
 
-def process_directory(directory_path):
+def process_directory_concurrently(directory_path):
     counter = 0
     for root, dirs, files in os.walk(directory_path):
-        with ThreadPoolExecutor(max_workers=10) as executor:
+        with ThreadPoolExecutor(max_workers=14) as executor:
             futures = []
             for file in files:
                 if "Main" in file or "Helper" in file:
                     continue
                 elif file.startswith("J") and file.endswith(".java"):
                     file_path = os.path.join(root, file)
-                    if ("CWE89" in file_path
-                            or "CWE78" in file_path
-                            #or "CWE476" in file_path
-                            #or "CWE190" in file_path
-                    ):
-                        counter += 1
-                        print(counter)
-                        future = executor.submit(run_prompt, file_path)
-                        futures.append(future)
+                    counter += 1
+                    print("x-count", counter)
+                    future = executor.submit(run_prompt, file_path)
+                    futures.append(future)
 
             # Wait for all tasks to complete
             for future in futures:
                 future.result()
 
 
+def process_directory(directory_path):
+    for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                if "Main" in file or "Helper" in file:
+                    continue
+                elif file.startswith("J") and file.endswith(".java"):
+                    file_path = os.path.join(root, file)
+                    run_prompt(file_path)
+
+
 if __name__ == "__main__":
     load_dotenv(find_dotenv())
 
-    file = os.environ.get("DATASET_DIRECTORY_ROOT") + "\\src\\testcases\\CWE23_Relative_Path_Traversal\\J18272.java"
-    runner = SelfReflectionPromptRunner(file, "dataflow_analysis_prompt")
-    runner.run_prompt()
+    #file = os.environ.get("DATASET_DIRECTORY_ROOT") + "\\src\\testcases\\CWE190_Integer_Overflow\\s02\\J14351.java"
+    #runner = CriticiseRefinePromptRunner(file, "basic_prompt_rci", "basic_prompt_rci_criticise", "basic_prompt_rci_improve")
+    #runner.run_prompt()
 
-    #dataset_root = os.environ.get("DATASET_DIRECTORY_ROOT")
-    #folder = os.path.join(dataset_root, "src", "testcases")
-    #process_directory(folder)
+    dataset_root = os.environ.get("DATASET_DIRECTORY_ROOT")
+    folder = os.path.join(dataset_root, "src", "testcases")
+    process_directory_concurrently(folder)
