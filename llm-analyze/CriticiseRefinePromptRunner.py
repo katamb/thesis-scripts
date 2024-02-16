df5c2cc2c1f25989162170b18414ddd8045b67fc
KarlTamberg <tamberg.karl@gmail.com>
2024-02-16 10:52:14 +0200

    Big changes to the runner for cost savings;

--- llm-analyze/CriticiseRefinePromptRunner.py
+++ llm-analyze/CriticiseRefinePromptRunner.py
@@ -1,7 +1,8 @@
+from typing import List, Tuple
 from BaseLlmRunner import BaseLlmRunner
 from langchain_community.callbacks import get_openai_callback
 from langchain.prompts import PromptTemplate
-from langchain.chains import ConversationChain
+from langchain.chains import LLMChain
 import threading
 import time
 import os
@@ -11,95 +12,89 @@ class CriticiseRefinePromptRunner(BaseLlmRunner):
     """
     The original prompt should be the same, but have a different file name, so we wouldn't overwrite previous results.
     Examples:
-        CriticiseRefinePromptRunner(file, "basic_prompt_rci", "basic_prompt_rci_criticise", "basic_prompt_rci_improve")
-        CriticiseRefinePromptRunner(file, "basic_prompt_refinement", "basic_prompt_refinement_feedback", "basic_prompt_refinement_refine")
+        CriticiseRefinePromptRunner(file, "basic_prompt_rci", [("basic_prompt_rci_criticise", "basic_prompt_rci_improve")])
+        CriticiseRefinePromptRunner(file, "basic_prompt_refinement", [("basic_prompt_refinement_feedback", "basic_prompt_refinement_refine")])
     """
-    def __init__(self, file_path, first_prompt_name, second_prompt_name, third_prompt_name, lock=threading.Lock()):
-        super().__init__(file_path, first_prompt_name, lock)
-        self.criticise_prompt = second_prompt_name
-        self.improve_prompt = third_prompt_name
-        self.criticise_folder_path = os.path.join(self.base_result_path, self.criticise_prompt)
-        self.improve_folder_path = os.path.join(self.base_result_path, self.improve_prompt)
-        if not os.path.exists(self.criticise_folder_path):
-            os.makedirs(self.criticise_folder_path, exist_ok=True)
-        if not os.path.exists(self.improve_folder_path):
-            os.makedirs(self.improve_folder_path, exist_ok=True)
+
+    def __init__(self,
+                 file_path: str,
+                 initial_prompt: str,
+                 improvement_prompts: List[Tuple[str, str]],
+                 lock: threading.Lock = threading.Lock()
+                 ):
+        super().__init__(file_path, initial_prompt, lock)
+        self.improvement_prompts = improvement_prompts
+
+    def get_folder_path(self, prompt_name):
+        folder_path = os.path.join(self.base_result_path, prompt_name)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path, exist_ok=True)
+        return folder_path
+
+    @staticmethod
+    def call_llm(chain: LLMChain) -> Tuple[str, float, int, int, int, float]:
+        with get_openai_callback() as cb:
+            start = time.time()
+            llm_response = chain.invoke(input={})
+            end = time.time()
+            total_tokens = cb.total_tokens
+            completion_tokens = cb.completion_tokens
+            prompt_tokens = cb.prompt_tokens
+            cost = cb.total_cost
+            time_spent = end - start
+            print(llm_response)
+        return llm_response["text"], time_spent, total_tokens, completion_tokens, prompt_tokens, cost
 
     def run_prompt(self):
-        self.validate()
-        conversation = ConversationChain(llm=self.llm)
-        code = self.load_file_content()
+        self.validate()  # Fail early if something is not properly defined
+        code = self.load_file_content()  # Load the code sample from dataset
+        initial_human_input = self.load_prompt_from_file(self.prompt_name).replace("{code}", code)
+        initial_template = f"""The following is a conversation between a human and an AI security analyst. If the AI security analyst does not know the answer to a question, it truthfully says it does not know.\n
+Human: {initial_human_input}
+AI: """
 
-        ## first call ##
-        first_template = self.load_prompt_from_file(self.prompt_name)
-        first_prompt = PromptTemplate.from_template(template=first_template, partial_variables={"code": code})
-        with get_openai_callback() as cb1:
-            start = time.time()
-            llm_response_1 = conversation.predict(input=first_prompt.format_prompt().text)
-            end = time.time()
-            total_tokens_1 = cb1.total_tokens
-            completion_tokens_1 = cb1.completion_tokens
-            prompt_tokens_1 = cb1.prompt_tokens
-            cost_1 = self.calculate_cost(cb1.prompt_tokens, cb1.completion_tokens)  # cb1.total_cost
-            time_spent_1 = end - start
-            print(llm_response_1)
-
+        ## initial call ##
+        initial_prompt = PromptTemplate(template=initial_template)
+        initial_chain = LLMChain(llm=self.llm, prompt=initial_prompt)
+        llm_response_1, time_spent_1, total_tokens_1, completion_tokens_1, prompt_tokens_1, cost_1 = self.call_llm(initial_chain)
         # save first call results
         tokens_used_1 = f"total_tokens: {total_tokens_1}, completion_tokens: {completion_tokens_1}, prompt_tokens: {prompt_tokens_1}"
         cwes = self.get_cwes(llm_response_1)
         self.save_result_row(self.prompt_name, len(cwes) != 0, cwes, time_spent_1, tokens_used_1, cost_1)
-
         # save first call LLM response for audit trail
         with open(os.path.join(self.result_folder_path, self.get_file_name()), "w") as r:
             r.write(llm_response_1)
 
-        ## second call ##
-        second_template = self.load_prompt_from_file(self.criticise_prompt)
-        second_prompt = PromptTemplate.from_template(second_template)
-        with get_openai_callback() as cb2:
-            start = time.time()
-            llm_response_2 = conversation.predict(input=second_prompt.format_prompt().text)
-            end = time.time()
-            total_tokens_2 = cb2.total_tokens
-            completion_tokens_2 = cb2.completion_tokens
-            prompt_tokens_2 = cb2.prompt_tokens
-            cost_2 = self.calculate_cost(cb2.prompt_tokens, cb2.completion_tokens)  # cb2.total_cost
-            time_spent_2 = end - start
-            print(llm_response_2)
+        ## second and third calls ##
+        for criticise_prompt_name, improve_prompt_name in self.improvement_prompts:
+            # second call
+            criticise_template = initial_template + llm_response_1 + "\nHuman: " + self.load_prompt_from_file(criticise_prompt_name) + "\nAI: "
+            criticise_prompt = PromptTemplate.from_template(criticise_template)
+            criticise_chain = LLMChain(llm=self.llm, prompt=criticise_prompt)
+            llm_response_2, time_spent_2, total_tokens_2, completion_tokens_2, prompt_tokens_2, cost_2 = self.call_llm(criticise_chain)
+            # save second call LLM response for audit trail
+            with open(os.path.join(self.get_folder_path(criticise_prompt_name), self.get_file_name()), "w") as r:
+                r.write(llm_response_2)
 
-        # save second call LLM response for audit trail
-        with open(os.path.join(self.criticise_folder_path, self.get_file_name()), "w") as r:
-            r.write(llm_response_2)
-
-        ## third (and final) call ##
-        third_template = self.load_prompt_from_file(self.improve_prompt)
-        third_prompt = PromptTemplate.from_template(third_template)
-        with get_openai_callback() as cb3:
-            start = time.time()
-            llm_response_3 = conversation.predict(input=third_prompt.format_prompt().text)
-            end = time.time()
-            total_tokens_3 = cb3.total_tokens
-            completion_tokens_3 = cb3.completion_tokens
-            prompt_tokens_3 = cb3.prompt_tokens
-            cost_3 = self.calculate_cost(cb3.prompt_tokens, cb3.completion_tokens)  # cb3.total_cost
-            time_spent_3 = end - start
-            print(llm_response_3)
-
-        # save third call results
-        tokens_used_sum = (f"total_tokens: {self.safe_int_addition(total_tokens_1, total_tokens_2, total_tokens_3)}, "
-                           f"completion_tokens: {self.safe_int_addition(completion_tokens_1, completion_tokens_2, completion_tokens_3)}, "
-                           f"prompt_tokens: {self.safe_int_addition(prompt_tokens_1, prompt_tokens_2, prompt_tokens_3)}")
-        is_vulnerable = True
-        cwe_ids = self.get_cwes(llm_response_3)
-        if cwe_ids == "":
-            is_vulnerable = False
-        time_spent_sum = self.safe_float_addition(time_spent_1, time_spent_2, time_spent_3)
-        cost_sum = self.safe_float_addition(cost_1, cost_2, cost_3)
-        self.save_result_row(self.improve_prompt, is_vulnerable, cwe_ids, time_spent_sum, tokens_used_sum, cost_sum)
-
-        # save third call LLM response for audit trail
-        with open(os.path.join(self.improve_folder_path, self.get_file_name()), "w") as r:
-            r.write(llm_response_3)
+            # third (and final) call
+            improve_template = criticise_template + llm_response_2 + "\nHuman: " + self.load_prompt_from_file(improve_prompt_name) + "\nAI: "
+            improve_prompt = PromptTemplate.from_template(improve_template)
+            improve_chain = LLMChain(llm=self.llm, prompt=improve_prompt)
+            llm_response_3, time_spent_3, total_tokens_3, completion_tokens_3, prompt_tokens_3, cost_3 = self.call_llm(improve_chain)
+            # save third call results
+            tokens_used_sum = (f"total_tokens: {self.safe_int_addition(total_tokens_1, total_tokens_2, total_tokens_3)}, "
+                               f"completion_tokens: {self.safe_int_addition(completion_tokens_1, completion_tokens_2, completion_tokens_3)}, "
+                               f"prompt_tokens: {self.safe_int_addition(prompt_tokens_1, prompt_tokens_2, prompt_tokens_3)}")
+            is_vulnerable = True
+            cwe_ids = self.get_cwes(llm_response_3)
+            if cwe_ids.strip() == "":
+                is_vulnerable = False
+            time_spent_sum = self.safe_float_addition(time_spent_1, time_spent_2, time_spent_3)
+            cost_sum = self.safe_float_addition(cost_1, cost_2, cost_3)
+            self.save_result_row(improve_prompt_name, is_vulnerable, cwe_ids, time_spent_sum, tokens_used_sum, cost_sum)
+            # save third call LLM response for audit trail
+            with open(os.path.join(self.get_folder_path(improve_prompt_name), self.get_file_name()), "w") as r:
+                r.write(llm_response_3)
 
         return llm_response_3
 
@@ -124,8 +119,8 @@ class CriticiseRefinePromptRunner(BaseLlmRunner):
         return vulnerabilities.strip()
 
     def validate(self):
-        first_template = self.load_prompt_from_file(self.prompt_name)
-        second_template = self.load_prompt_from_file(self.criticise_prompt)
-        third_template = self.load_prompt_from_file(self.improve_prompt)
-        if first_template.strip() == "" or second_template.strip() == "" or third_template.strip() == "":
-            raise Exception("One of the templates is not defined!")
+        for criticise_prompt, improve_prompt in self.improvement_prompts:
+            c_template = self.load_prompt_from_file(criticise_prompt)
+            i_template = self.load_prompt_from_file(improve_prompt)
+            if c_template.strip() == "" or i_template.strip() == "":
+                raise Exception("One of the templates is not defined!")
